2110bd3feacbac09961153b43ddff48cae2adc2c
Elle Kiser <shealtiely3@gmail.com>
2026-07-08 21:19:03 -0400

    Add retrieve_incidents function skeleton

---
+++ src/fetch.py
@@ -0,0 +1,8 @@
+def retrieve_incidents():
+    """Retrieve the last 7 days of security incidents from the ACLED API
+    Returns:
+       dict: Incident data returned by ACLED API in JSON format
+    Raises:
+        Exception: If request fails after all retry attempts
+    """
+    return{}
